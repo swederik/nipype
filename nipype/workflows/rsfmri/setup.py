c705bb5b4a0ff54551fa59be627472c162542e32
Erik Ziegler <erik.ziegler@ulg.ac.be>
2012-06-27 14:44:07 +0200

    Workflow for creating connectivity graphs from ICA results and resting state fMRI

--- nipype/workflows/rsfmri/setup.py
+++ nipype/workflows/rsfmri/setup.py
@@ -5,6 +5,7 @@ def configuration(parent_package='',top_path=None):
 
     config = Configuration('rsfmri', parent_package, top_path)
     config.add_subpackage('fsl')
+    config.add_subpackage('coma')
 
     return config
 
