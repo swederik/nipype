2dd45ebff032c8b20c491567cabd953d6f57b4b7
Erik Ziegler <erik.ziegler@ulg.ac.be>
2012-11-26 14:04:24 +0100

    Interfaces for node removal

--- nipype/interfaces/cmtk/__init__.py
+++ nipype/interfaces/cmtk/__init__.py
@@ -1,5 +1,5 @@
 from .cmtk import ROIGen, CreateMatrix, CreateNodes, TractsBetween, NetworkBasedROIFiltering, NetworkBasedStatsFiltering, TractsInImage
-from .nx import NetworkXMetrics, AverageNetworks, DifferenceGraph, Network2NiftiImage, CommonEdges, ThresholdNetwork
+from .nx import NetworkXMetrics, AverageNetworks, DifferenceGraph, Network2NiftiImage, CommonEdges, ThresholdNetwork, RemoveUnconnectedNodes, RemoveNodesByPhrase
 from .parcellation import Parcellate
 from .convert import CFFConverter, MergeCNetworks
 from .nbs import NetworkBasedStatistic
